3190753f7176197188d8f3eb055b2fe3952268cc
Cristinahb <achbocourt@gmail.com>
2023-08-13 15:01:04 +0200

    subir dia 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: "", component: HomeComponent},
+  { path: "header", component: HeaderComponent},
+  { path: "home", component: HomeComponent},
+  { path: "register", component: RegisterComponent},
+  { path: "footer", component: FooterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
