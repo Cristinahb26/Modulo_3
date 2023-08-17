da21da78cc64e2d4f788be1b95d495667126e6d4
Cristinahb <achbocourt@gmail.com>
2023-08-17 12:25:26 +0200

    subir dia 2 terminado

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+
+     public user: User;
+
+  constructor(){
+
+      this.user = new User(1, "Zoe", "Fernandez", "zoeFernandez@gmail.com", "https://images.unsplash.com/photo-1616002411355-49593fd89721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhcmElMjBkZSUyMG11amVyfGVufDB8fDB8fHww&w=1000&q=80", "Zoef1234")
+  
+    }
+
+   public enviar(name:string, last_name:string, email:string, photo:string){
+
+      this.user.name = name;
+      this.user.last_name = last_name;
+      this.user.email = email;
+      this.user.photo = photo;
+
+   }
 
 }
