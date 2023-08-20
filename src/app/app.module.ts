b599051d6cb6f83a4735f67d69740380a8314f2b
Cristinahb <achbocourt@gmail.com>
2023-08-20 14:12:19 +0200

    subir dia3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormRegisterComponent } from './component/form-register/form-register.c
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { BooksComponent } from './pages/books/books.component';
+import { RefCodigoPipe } from './pipes/ref-codigo.pipe';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { BooksComponent } from './pages/books/books.component';
     RegisterComponent,
     ProfileComponent,
     BooksComponent,
+    RefCodigoPipe,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -14,17 +14,17 @@ export class BooksComponent {
 
     this.books = [
 
-      new Book(1, 0, "El Conde de Montecristo", "Tapa Blanda", "Alejandro Dumas", 15.99, "https://covers.alibrate.com/b/59872e92cba2bce50c1b7929/06d27f27-09cf-4135-a431-1d6f54c184e3/share"),
-      new Book(2, 0, "El Elemento", "tapa blanda", "Ken Robinson", 12.99, "https://tecreolibreria.com/386-medium_default/el-elemento--ken-robinson.jpg"),
-      new Book(3, 0, "El Poder de Confiar en Ti", "Tapa Tlanda", "Curro Cañete", 16, "https://e00-telva.uecdn.es/assets/multimedia/imagenes/2019/12/03/15753839869322.jpg"),
-      new Book(4, 0, "El Ángel de la Ciudad", "tapa blanca", "Eva G. Sáenz de Urturi", 13.50, "https://i.blogs.es/5e4c0e/1366_2000-3-/450_1000.jpeg")
+      new Book("El Conde de Montecristo", "Tapa Blanda", "Alejandro Dumas", 15.99, "https://covers.alibrate.com/b/59872e92cba2bce50c1b7929/06d27f27-09cf-4135-a431-1d6f54c184e3/share", 1, 0),
+      new Book("El Elemento", "Tapa Blanda", "Ken Robinson", 12.99, "https://tecreolibreria.com/386-medium_default/el-elemento--ken-robinson.jpg", 2, 0),
+      new Book("El Poder de Confiar en Ti", "Tapa Blanda", "Curro Cañete", 16, "https://e00-telva.uecdn.es/assets/multimedia/imagenes/2019/12/03/15753839869322.jpg", 3, 0),
+      new Book("El Ángel de la Ciudad", "Tapa Blanca", "Eva G. Sáenz de Urturi", 13.50, "https://i.blogs.es/5e4c0e/1366_2000-3-/450_1000.jpeg", 4, 0)
 
     ]
   }
 
   addBook(id_book:number, title:string, type:string, author:string, price:number, photo:string) {
 
-    let newBook = new Book(0, id_book, title, type, author, price, photo,);
+    let newBook = new Book(title, type, author, price, photo, id_book);
     this.books.push(newBook);
 
   } 
